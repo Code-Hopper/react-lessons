360791ff4e902b015ee92e7c27b3f29d3511df00
“code-hopper” <ameykhondekar01@gmail.com>
2026-03-05 10:22:20 +0530

    updates

---
+++ basic/new2.js
@@ -0,0 +1,60 @@
+// // date class
+
+// let date = new Date()
+
+// // getter(to get info) and setter(to set info)
+// date.setDate(1)
+// date.setMonth(0)
+// date.setFullYear(1970)
+// date.setHours(0)
+// date.setMinutes(0)
+// date.setSeconds(0)
+// date.setMilliseconds(0)
+
+// console.log(date.getTime()) // milliseconds 1 jan 1970 to till date
+// console.log(date.getMonth())
+// console.log(date.getDate())
+// console.log(date.getDay())
+// console.log(date.getFullYear())
+// console.log(date.getMinutes())
+// console.log(date.getSeconds())
+// console.log(date.getHours())
+
+// Math
+
+// console.log(Math.E)
+// console.log(Math.LOG10E)
+// console.log(Math.LN10)
+// console.log(Math.SQRT2)
+// console.log(Math.PI.toFixed(5)) // will limit number after decimal but will convert into string
+
+// console.log(Math.min(1,2,3,4,6,7,8,9,10))
+// console.log(Math.max(1,2,3,4,6,7,8,9,10))
+
+// let numbers = [1,2,3,4,5,6,7,8]
+
+// console.log(Math.max(...numbers))
+// console.log(numbers)
+// console.log(...numbers)
+
+// console.log(Math.round(5.12))
+// console.log(Math.round(5.51))
+
+// console.log(Math.floor(5.99999))
+// console.log(Math.ceil(5.000001))
+
+// // 3.5 -> 4 | 3.2 -> 3
+
+// console.log(Math.sqrt(64))
+// console.log(Math.cbrt(27))
+
+// console.log(Math.pow(2,7))
+
+// console.log(Math.)
+
+// Math.tan
+
+// console.log(Math.round(Math.random()*6))
+// console.log(Math.floor(((Math.random()*5)+1)))
+
+// sync + async / exception handling  
